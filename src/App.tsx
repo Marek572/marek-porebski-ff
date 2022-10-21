31360c64a6ebfc8d0a3593a9c8a9654d521b84bb
Marek Porębski <marek.bp@poczta.fm>
2022-10-21 15:35:40 +0200

    lab-03

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
-import {FC} from "react"
-import {BrowserRouter,Routes,Route,Link} from "react-router-dom";
-import {Home} from "./components/Routes/Home/home";
-import {Posts} from "./components/Routes/Posts/post";
+import { FC } from "react"
+
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Home } from "./components/Routes/Home/home";
+import { Posts } from "./components/Routes/Posts/post";
+import { GlobalStyle } from './styleHelpers/global'
+import { Layout } from "./components/Common/Layout";
 
 export const App: FC = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="posts">Posts</Link>
-      </div>
-      <Routes>
-        <Route index element={<Home />}></Route>
-        <Route path="posts" element={<Posts />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route index element={<Home />}></Route>
+            <Route path="posts" element={<Posts />}></Route>
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </>
   );
 }
